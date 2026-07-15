256e4c8b24e6d4aaeb43613c0a8528eac6ee572e
frtipan <frtipan1@espe.edu.ec>
2026-07-14 21:48:23 -0500

    DAST

---
+++ tmp-send-telegram.py
@@ -0,0 +1,15 @@
+import json
+import urllib.request
+
+bot_token = '8846138406:AAGi34fd5fREf7PcPBT-SHxTTNVJGeKKgFY'
+chat_id = '8750807638'
+payload = {'chat_id': chat_id, 'text': 'Prueba directa desde Python OK'}
+data = json.dumps(payload).encode('utf-8')
+req = urllib.request.Request(
+    f'https://api.telegram.org/bot{bot_token}/sendMessage',
+    data=data,
+    headers={'Content-Type': 'application/json'},
+    method='POST',
+)
+with urllib.request.urlopen(req, timeout=20) as resp:
+    print(resp.read().decode('utf-8'))
